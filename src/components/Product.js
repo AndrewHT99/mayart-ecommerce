224281d43ddc24b77222acad2190677fe5896f2c
Andrés Hernández Tinoco <andreshernandeztinoco@hotmail.com>
2021-10-24 03:35:31 -0500

    H1,H2,H3,H4 & btn added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Btn from './atoms/Btn';
+import P from './atoms/P';
+import H1 from './atoms/H1';
+import H2 from './atoms/H2';
+import H3 from './atoms/H3';
+import H4 from './atoms/H4';
 
 const Product = () => {
     return (
         <div id="product-card">
-            prueba
+            <Btn handleClick={()=>alert("Holo")} text="Try me"/>
+            <P text="Soy un párrafo de prueba ché"/>
+            <H1 text="Soy un h1 ché"/>
+            <H2 text="Soy un h2 ché"/>
+            <H3 text="Soy un h3 ché"/>
+            <H4 text="Soy un h4 ché"/>
         </div>
     )
 }
---
+++ src/components/atoms/H4.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const H4 = ({ text }) => {
+    return (
+        <h4>{text}</h4>
+    )
+}
+
+export default H4;
